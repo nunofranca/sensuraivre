2d3cbc9ab36c903f2c11f892e3680f2e7b6d5a86
Nuno França <alberto.oliveira.ext@dasa.com.br>
2022-02-12 13:09:37 -0300

    ..

--- resources/views/site/materia/index.blade.php
+++ resources/views/site/materia/index.blade.php
@@ -1,5 +1,5 @@
 @extends('site.master.index')
-
+@section('title', $post->title)
 @section('content')
     <main>
         <!-- About US Start -->
@@ -9,15 +9,7 @@
                 <div class="row">
                     <div class="col-lg-12">
                         <div class="trending-tittle">
-                            <strong>Trending now</strong>
-                            <!-- <p>Rem ipsum dolor sit amet, consectetur adipisicing elit.</p> -->
-                            <div class="trending-animated">
-                                <ul id="js-news" class="js-hidden">
-                                    <li class="news-item">Bangladesh dolor sit amet, consectetur adipisicing elit.</li>
-                                    <li class="news-item">Spondon IT sit amet, consectetur.......</li>
-                                    <li class="news-item">Rem ipsum dolor sit amet, consectetur adipisicing elit.</li>
-                                </ul>
-                            </div>
+                            <strong>{{$post->category->name}}</strong>
 
                         </div>
                     </div>
